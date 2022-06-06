6cca3ba34baccdbcd466adc8a682d96f1272d348
Dmytro Tkachenko <tkachenko.com@gmail.com>
2022-06-06 11:18:47 +0300

    uncommented api address, demodata still availiable

--- src/reports/api-reports.js
+++ src/reports/api-reports.js
@@ -4,14 +4,17 @@ import { demodata } from './demoData';
 const list = async (signal) => {
   try {
     // let response = await fetch(`${SERVER_URL}/api/reports/`, {
-    //   // let response = await fetch('http://*.**.**.**:3000/api/reports/', {
-    //   method: 'GET',
-    //   signal: signal,
-    // })
-    // return await response.json()
-    return demodata;
+    // let response = await fetch('http://*.**.**.**:3000/api/reports/', {
+    let response = await fetch('http://3.84.53.46:3000/api/reports/', {
+      method: 'GET',
+      signal: signal,
+    })
+    return await response.json()
+
   } catch (err) {
     console.log(err)
+    console.log("TEST: demo data loaded")
+    return demodata;
   }
 }
 
